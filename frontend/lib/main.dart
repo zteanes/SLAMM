6c5c6441195cc76f7f12d691cb0544e53436fcd7
Alex Charlot <acharlot2002@gmail.com>
2024-10-28 18:03:46 -0400

    still working with themes, can not get colors to change using Theme.of(context)

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -3,6 +3,8 @@ import 'package:frontend/settings.dart';
 import 'constants.dart';
 import 'analytics_screen.dart';
 import 'camera.dart';
+import 'theme.dart';
+import 'welcome.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,18 +13,15 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //ThemeMode themeMode = ThemeMode.system;
     return MaterialApp(
       title: 'Welcome Screen',
-      theme: ThemeData(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: bgColor,
-      ),
       home: const WelcomeScreen(),
+      theme: lightmode,
+      darkTheme: darkmode,
       routes: {
         "analytics": (context) => const AnalyticsScreen(),
         "welcome": (context) => const WelcomeScreen(),
@@ -33,84 +32,84 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WelcomeScreen extends StatelessWidget {
-  const WelcomeScreen({super.key});
+// class WelcomeScreen extends StatelessWidget {
+//   const WelcomeScreen({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Stack(
-      children: [
-        Positioned.fill(
-          child: Opacity(
-            opacity: 0.6,
-            child: Image.asset('assets/images/temp-splash.jpg', fit: BoxFit.cover),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(height: 200),
-              const Text(
-                'Welcome to', 
-                style: TextStyle(color: primaryColor, fontSize: 36),
-              ),
-              const Text(
-                'SLAMM', 
-                style: TextStyle(color: primaryColor, fontSize: 48, fontWeight: FontWeight.bold),
-              ),
-              const SizedBox(height: 20),
-              const Padding(
-                padding: EdgeInsets.only(left: 45, right: 45),
-                child: Text(
-                  'Sign Language Analytics and Mobile Machine Learning',
-                  style: TextStyle(color: Colors.white, fontSize: 24),
-                  textAlign: TextAlign.center,
-                  ),
-              ),
-              const SizedBox(height: 20),
-              SizedBox(
-                width: 300,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: primaryColor,
-                    padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: const Text("Let's Get Started", style: TextStyle(fontSize: 20, color: Colors.white)), 
-                  // open our next page, the analytics page
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => const AnalyticsScreen()),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(height: 15),
-              SizedBox(
-                width: 300,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                  ),
-                  child: const Text("Sign In", style: TextStyle(fontSize: 20, color: primaryColor)),
-                  onPressed: () {},
-                ),
-              )
-            ],
-          ),
-        ),
-      ], 
-    ),
-  );}
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//       children: [
+//         Positioned.fill(
+//           child: Opacity(
+//             opacity: 0.6,
+//             child: Image.asset('assets/images/temp-splash.jpg', fit: BoxFit.cover),
+//           ),
+//         ),
+//         Align(
+//           alignment: Alignment.center,
+//           child: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               const SizedBox(height: 200),
+//               Text(
+//                 'Welcome to', 
+//                 style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 36),
+//               ),
+//               Text(
+//                 'SLAMM', 
+//                 style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 48, fontWeight: FontWeight.bold),
+//               ),
+//               const SizedBox(height: 20),
+//               Padding(
+//                 padding: EdgeInsets.only(left: 45, right: 45),
+//                 child: Text(
+//                   'Sign Language Analytics and Mobile Machine Learning',
+//                   style: TextStyle(color: Theme.of(context).colorScheme.secondary, fontSize: 24),
+//                   textAlign: TextAlign.center,
+//                   ),
+//               ),
+//               const SizedBox(height: 20),
+//               SizedBox(
+//                 width: 300,
+//                 child: ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Theme.of(context).colorScheme.primary,
+//                     padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(30),
+//                     ),
+//                   ),
+//                   child: Text("Let's Get Started", style: TextStyle(fontSize: 20, color: Theme.of(context).colorScheme.secondary)), 
+//                   // open our next page, the analytics page
+//                   onPressed: () {
+//                     Navigator.push(
+//                       context,
+//                       MaterialPageRoute(builder: (context) => const AnalyticsScreen()),
+//                     );
+//                   },
+//                 ),
+//               ),
+//               const SizedBox(height: 15),
+//               SizedBox(
+//                 width: 300,
+//                 child: ElevatedButton(
+//                   style: ElevatedButton.styleFrom(
+//                     backgroundColor: Theme.of(context).colorScheme.secondary,
+//                     padding: const EdgeInsets.symmetric(horizontal: 80, vertical: 20),
+//                     shape: RoundedRectangleBorder(
+//                       borderRadius: BorderRadius.circular(30),
+//                     ),
+//                   ),
+//                   child: Text("Sign In", style: TextStyle(fontSize: 20, color: Theme.of(context).colorScheme.primary)),
+//                   onPressed: () {},
+//                 ),
+//               )
+//             ],
+//           ),
+//         ),
+//       ], 
+//     ),
+//   );}
+// }
 
