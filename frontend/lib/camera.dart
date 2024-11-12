3afdee489f22bc5c59fee1a7c9305324993d4452
Alex Charlot <acharlot2002@gmail.com>
2024-11-11 19:58:51 -0500

    camera switching now implemented, scaling messed up

--- frontend/lib/camera.dart
+++ frontend/lib/camera.dart
@@ -7,7 +7,7 @@ import 'dart:math' as math;
 
 int frontCamera = 1;
 int backCamera = 0;
-int cameraInUse = frontCamera;
+
 
 class CameraScreen extends StatefulWidget {
   const CameraScreen({super.key});
@@ -16,95 +16,62 @@ class CameraScreen extends StatefulWidget {
   State<CameraScreen> createState() => CameraScreenState();
 }
 
-// CameraApp is a StatefulWidget because we may need to
-// change the state later, for example, to switch cameras or take pictures
-class CameraApp extends StatefulWidget {
-  const CameraApp({super.key});
-  @override
-  CameraAppState createState() => CameraAppState();
-}
+// // CameraApp is a StatefulWidget because we may need to
+// // change the state later, for example, to switch cameras or take pictures
+// class CameraApp extends StatefulWidget {
+//   const CameraApp({super.key});
+//   @override
+//   CameraScreenState createState() => CameraAppState();
+// }
 
 // CameraAppState is the state of the CameraApp widget
-class CameraAppState extends State<CameraApp> {
+class CameraScreenState extends State<CameraScreen> {
   late CameraController controller;
+  bool isCameraFront = true;
   @override
   void initState() {
     super.initState();
-    // change from front camera (1) to back camera (0)
-    print("cameraInUse: $cameraInUse");
-    controller = CameraController(cameras[frontCamera], ResolutionPreset.medium,);
+    _initializeCamera(backCamera);
+  }
+
+  void _initializeCamera(int cameraPos) {
+    controller = CameraController(cameras[cameraPos], ResolutionPreset.medium,);
     controller.initialize().then((_) {
       if (!mounted) {
         return;
       }
       setState(() {});
     });
-  }
-
-  // dispose of the controller when the widget is removed
-  // to prevent memory leaks
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
-
-  // build the camera preview
-  // if the controller is not initialized, return an empty container
-  @override
-  Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return CameraPreview(controller);
   }
 
   void switchCamera() {
-    // switch from front camera to back camera
-    if (cameraInUse == frontCamera) {
-      print("switching to back camera");
-      cameraInUse = backCamera;
-    } else {
-      print("switching to front camera");
-      cameraInUse = frontCamera;
-    }
-    // dispose old one and initialize new controller
-    controller.dispose();
-    controller = CameraController(cameras[cameraInUse], ResolutionPreset.medium,);
-    controller.initialize().then((_) {
-      if (!mounted) {
-        return;
-      }
-      setState(() {});
+    setState(() {
+      isCameraFront = !isCameraFront;
     });
-  }
-}
+    controller.dispose(); // dispose of the current controller
+    _initializeCamera(isCameraFront ? frontCamera:backCamera); // initialize a new controller
 
-class CameraScreenState extends State<CameraScreen> {
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
         children: [
-            if (cameraInUse == frontCamera) 
-              Transform(transform: Matrix4.rotationY(0), 
-                        alignment: Alignment.center, 
-                        child: const SizedBox.expand( child: CameraApp(),),
-              ) 
-            else  // rotate the camera preview 180 degrees to make it feel natural
-              Transform(transform: Matrix4.rotationY(0), 
-                        alignment: Alignment.center, 
-                        child: const SizedBox.expand( child: CameraApp(),),
-              ),
+          if(controller.value.isInitialized)
+            CameraPreview(controller),
           Align( // button to switch front and back cameras
             alignment: Alignment.topRight,
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: ElevatedButton(
                 onPressed: () {
-                  setState(() {
-                    CameraAppState().switchCamera();
-                  });
+                  switchCamera();
                 },
                 style: ElevatedButton.styleFrom(
                   shape: const CircleBorder(),
@@ -130,52 +97,8 @@ class CameraScreenState extends State<CameraScreen> {
               ),
             ),
           )
-
-          // body: Stack(
-          //   children: [
-          //     Positioned.fill(
-          //       child: Opacity(
-          //         opacity: 0.6,
-          //         child: Image.asset('assets/images/temp-splash.jpg', fit: BoxFit.cover),
-          //       ),
-          //     ),
-          //     Align(
-          //       alignment: Alignment.center,
-          //       child: Column(
-          //         mainAxisAlignment: MainAxisAlignment.center,
-          //         children: [
-          //           Text(
-          //             'Camera',
-          //             style: TextStyle(color: Theme.of(context).colorScheme.primary, fontSize: 36),
-          //           ),
-          //           const SizedBox(height: 200),
-          //           const SizedBox(height: 20),
-          //           SizedBox(
-
-          //           )
-          // SizedBox(
-          //   width: 300,
-          //   child: ElevatedButton(
-          //     style: ElevatedButton.styleFrom(
-          //       backgroundColor: Colors.white,
-          //       padding: const EdgeInsets.symmetric(horizontal: 60, vertical: 20),
-          //       shape: RoundedRectangleBorder(
-          //         borderRadius: BorderRadius.circular(30),
-          //       ),
-          //     ),
-          //     child: const Text("Go back", style: TextStyle(fontSize: 20, color: Colors.black)),
-          //     // go back to the welcome/landing page
-          //     onPressed: () {
-          //       Navigator.pushNamed(context, "welcome");
-          //     },
-          //   ),
-          // ),
         ],
       ),
-      //     ),
-      //   ],
-      // ),
-      // add our bottom tab bar
       bottomNavigationBar: BottomAppBar(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
