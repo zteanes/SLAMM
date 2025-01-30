243fa7f1a029311d60c2632d598f86ca0eba1890
Alex Charlot <acharlot2002@gmail.com>
2025-01-30 18:34:12 -0500

    video storing to temp folder. Made changes to settings.gradle, build.gradle, and gradle-wrapper to get the program to run. Bugs: -still saving to camera roll (suspect emulator error) -last video recoded does not delete when app is closed. -warnings upon build.

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,13 +8,11 @@ import Foundation
 import file_selector_macos
 import gal
 import path_provider_foundation
-import photo_manager
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   GalPlugin.register(with: registry.registrar(forPlugin: "GalPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
   FVPVideoPlayerPlugin.register(with: registry.registrar(forPlugin: "FVPVideoPlayerPlugin"))
 }
