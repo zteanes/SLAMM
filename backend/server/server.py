5e64b4572b9d90e616d6cd82705f188fbd0fdc37
zteanes <zteanes1@catamount.wcu.edu>
2025-02-20 09:42:23 -0500

    remove unnecessary functions

--- backend/server/server.py
+++ backend/server/server.py
@@ -199,34 +199,6 @@ def create_WLASL_dictionary():
                 value = split_list[1]
             wlasl_dict[key] = value
 
-# def load_TGCN_model():
-#     """
-#     Load the TGCN model from WLASL for communication with frontend.
-#     """
-#     # change root and subset accordingly.
-#     root = os.getcwd();
-#     trained_on = 'asl100'
-
-#     # config file for all information used to load model
-#     config_file = os.path.join(root, 'backend/TGCN/configs/{}.ini'.format(trained_on, trained_on))
-#     configs = Config(config_file)
-
-#     # necessary variables we get from the config
-#     num_samples = configs.num_samples
-#     hidden_size = configs.hidden_size
-#     drop_p = configs.drop_p
-#     num_stages = configs.num_stages
-
-#     # load the model
-#     log(Fore.CYAN + "Loading model...")
-#     model = GCN_muti_att(input_feature=num_samples * 2, hidden_feature=hidden_size,
-#                          num_class=int(trained_on[3:]), p_dropout=drop_p, 
-#                          num_stage=num_stages).cuda()
-#     log(Fore.CYAN + "Finish loading model!")
-
-#     # return the loaded model
-#     return model
-
 
 ########### end methods for debugging and loading model ###########
 
