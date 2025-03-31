a2d2eed07661c46a10f93623014216c416362a1c
zteanes <zteanes1@catamount.wcu.edu>
2025-03-31 17:58:00 -0400

    new analytics screen wip; pie chart almost done.

--- frontend/lib/analytics_screen.dart
+++ frontend/lib/analytics_screen.dart
@@ -11,6 +11,9 @@ import 'package:SLAMM/tabs_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
+// used for a pie chart 
+import 'package:fl_chart/fl_chart.dart';
+
 // how to change: 
 //  - add a pie chart with the most used words, listing the 5 most used words beside it
 //  - total number of words translated 
@@ -29,101 +32,18 @@ CollectionReference db = FirebaseFirestore.instance.collection('Users');
 // reference to the user authentication 
 final auth = FirebaseAuth.instance;
 
+
 class AnalyticsScreenState extends State<AnalyticsScreen> {
   @override
   void initState() {
     super.initState();
   }
 
+  // instance of the db to reference throughout this screen
   final DbService _db_service = DbService();
 
-  String getWords(String userName) {
-    return "Hi";
-  }
-
-  Container listOfStats(DocumentSnapshot? doc, List<String> words) {
-    return Container(
-      height: 600, // Set a fixed height for vertical scrolling // Set a fixed width for horizontal scrolling (adjust as needed)
-      decoration: BoxDecoration(
-        color: Colors.transparent,
-        borderRadius: BorderRadius.circular(10),
-      ),
-      child: Scrollbar( // Optional: Adds a scrollbar for better UX
-        child: ListView(
-          children: <Widget>[
-            Text("${doc?.get("firstName")}'s Analytics", 
-              style: const TextStyle(fontSize: 28, color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-
-            const Text("All Words Signed:",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-
-            for (var word in words)
-              getWord(word),
-
-            const Text("\nNumber of Words:",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-            getNumWords(words),
-            const Text("\nMost Used Word:",
-              style: TextStyle(fontSize: 30, color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
-            getMostUsedWord(words),
-            
-          ]
-        ),
-      ),
-    );
-  }
-
-  Text getNumWords(List<String> words) {
-    return Text("${words.length}",
-      style: const TextStyle(fontSize: 20, color: Colors.white),
-      textAlign: TextAlign.center,
-    );
-  }
-
-  Text getWord(String word){
-    return Text(word,
-      style: const TextStyle(fontSize: 20, color: Colors.white),
-      textAlign: TextAlign.center,
-    );
-  }
-
-  Text getMostUsedWord(List<String> words) {
-    Map<String, int> wordCount = {};
-    for (var word in words) { 
-      wordCount[word] = (wordCount[word] ?? 0) + 1;
-    }
-
-    String mostUsedWord = "";
-    int maxCount = 0;
-    
-    wordCount.forEach((word, int count) {
-      if (count > maxCount) {
-        maxCount = count;
-        mostUsedWord = word;
-      }
-    });
-
-    if (mostUsedWord == ""){
-      return const Text("No words signed yet!",
-          style: TextStyle(fontSize: 20, color: Colors.white),
-          textAlign: TextAlign.center,
-        );
-    } else{ 
-      return Text(mostUsedWord,
-          style: const TextStyle(fontSize: 20, color: Colors.white),
-          textAlign: TextAlign.center,
-        );
-    }
-  }
-
+  // screen is an entire scrollable container, with a static header of user's name 
+  // and a list of their analytics
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -141,14 +61,32 @@ class AnalyticsScreenState extends State<AnalyticsScreen> {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                //gets the number of users from the db and displays it on screen
+                // static header 
                 StreamBuilder(
                   stream: _db_service.getUser(auth.currentUser?.uid), // gets the user that is signed in
                   builder: (context, snapshot){
                     if (!snapshot.hasData) {
                       return const CircularProgressIndicator();
                     } else {
-                      // display the user's name from the db
+                      // reference of the data document to use 
+                      DocumentSnapshot<Object?>? doc = snapshot.data;
+
+                      return Text("${doc?.get("firstName")}'s Analytics", 
+                        style: const TextStyle(fontSize: 30, color: Colors.white),
+                        textAlign: TextAlign.center,
+                      );
+                    }
+                  }, 
+                ),
+
+                // gets the number of users from the db and displays it on screen
+                StreamBuilder(
+                  stream: _db_service.getUser(auth.currentUser?.uid), // gets the user that is signed in
+                  builder: (context, snapshot){
+                    if (!snapshot.hasData) {
+                      return const CircularProgressIndicator();
+                    } else {
+                      // reference of the data document to use
                       DocumentSnapshot<Object?>? doc = snapshot.data;
 
                       // get the list of words from the user document
@@ -167,4 +105,256 @@ class AnalyticsScreenState extends State<AnalyticsScreen> {
       bottomNavigationBar: const BottomTabBar(),
     );
   }
+
+
+  /// Creates a container that holds the list of analytics for the user.
+  /// 
+  /// @param doc: the user's document from the database
+  /// @param words: the list of words that the user has signed
+  /// @return a container that holds the list of analytics for the user
+  Container listOfStats(DocumentSnapshot? doc, List<String> words) {
+    return Container(
+      height: 600, // Set a fixed height for vertical scrolling (adjust as needed)
+      decoration: BoxDecoration(
+        color: Colors.transparent,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Scrollbar( 
+        child: ListView(
+          children: <Widget>[
+
+            // most common words signed with a pie chart
+            Text("Most Common Words Signed:",
+              style: TextStyle(fontSize: 26, color: Theme.of(context).primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            getMostCommonWords(words),
+
+            // number of words signed by the user
+            Text("\nNumber of Words:",
+              style: TextStyle(fontSize: 26, color: Theme.of(context).primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            getNumWords(words),
+            
+            // most used word by the user
+            Text("\nMost Used Word:",
+              style: TextStyle(fontSize: 26, color: Theme.of(context).primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            getMostUsedWord(words),
+            
+            // list of all words signed by the user
+            Text("All Words Signed:",
+              style: TextStyle(fontSize: 26, color: Theme.of(context).primaryColor),
+              textAlign: TextAlign.center,
+            ),
+            getAllWords(words),
+
+          ]
+        ),
+      ),
+    );
+  } // end ---listOfStats---
+
+
+  /// Creates a Text widget that displays the most common words signed by the user.
+  /// 
+  /// @param words: the list of words that the user has signed
+  /// @return a Text widget that displays the most common words signed by the user
+  Text getMostCommonWords(List<String> words) {
+    // map the count of each word
+    Map<String, int> wordCount = {};
+
+    // iterate the words with a map, incrementing the count of each word
+    for (var word in words) { 
+      wordCount[word] = (wordCount[word] ?? 0) + 1;
+    }
+
+    // sort the map by count of each word
+    wordCount = Map.fromEntries(wordCount.entries
+                        .toList()..sort((e1, e2) => e2.value.compareTo(e1.value)));
+
+    // get the top 5 most common words
+    List<String> mostCommonWords = [];
+    int count = 0;
+    while (count < 5 && count < wordCount.length) {
+      mostCommonWords.add("${wordCount.keys.elementAt(count)} (${wordCount.values.elementAt(count)})");
+      count++;
+    }
+
+    // if no words are signed, display a message
+    if (mostCommonWords.isEmpty){
+      return Text("No words signed yet!",
+          style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
+          textAlign: TextAlign.center,
+        );
+    }
+
+    // make a pie chart of the most common words
+    // return PieChart(
+    //   PieChartData(
+    //     sections: List.generate(mostCommonWords.length, (index) {
+    //       return PieChartSectionData(
+    //         color: Colors.primaries[index % Colors.primaries.length],
+    //         value: wordCount.values.elementAt(index).toDouble(),
+    //         title: mostCommonWords[index],
+    //         radius: 100,
+    //       );
+    //     }),
+    //   ),
+    // );
+
+    // return common words as both a pie chart and a list
+    return Text(mostCommonWords.toString().replaceAll("[", "").replaceAll("]", ""),
+      style: TextStyle(fontSize: 18, color: Theme.of(context).primaryColor),
+      textAlign: TextAlign.center,
+    );
+
+
+  }
+
+  /// Creates a Text widget that displays the number of words signed by the user.
+  /// 
+  /// @param words: the list of words that the user has signed
+  /// @return a Text widget that displays the number of words signed by the user
+  Text getNumWords(List<String> words) {
+    return Text("${words.length}",
+      style: TextStyle(fontSize: 18, color: Theme.of(context).primaryColor),
+      textAlign: TextAlign.center,
+    );
+  } // end getNumWords
+
+
+/// Creates a widget that displays all the words signed by the user.
+/// 
+/// @param words: the list of words that the user has signed
+/// @return a widget that displays all the words signed by the user
+Widget getAllWords(List<String> words) {
+  // ensure user has signed words, if not display a message
+  if (words.isEmpty) {
+    return Text(
+      "No words signed yet!",
+      style: TextStyle(fontSize: 20, color: Theme.of(context).primaryColor),
+      textAlign: TextAlign.center,
+    );
+  }
+
+  // display the words in a wrap widget
+  return LayoutBuilder(
+    builder: (context, constraints) {
+      // calculate the width of each column necessary
+      double columnWidth = constraints.maxWidth / 3;
+
+      // put everything in a wrap widget to display words in 3 columns 
+      return Wrap(
+        alignment: WrapAlignment.center,
+        spacing: 6,
+        runSpacing: 6,
+        children: words
+            .map((word) => SizedBox(
+                  width: columnWidth - 10,
+                  child: Text(
+                    word,
+                    style: TextStyle(fontSize: 18, color: Theme.of(context).primaryColor),
+                    textAlign: TextAlign.center,
+                  ),
+                ))
+            .toList(),
+      );
+    },
+  );
 }
+
+  /// Creates a Text widget that displays the most used word by the user.
+  /// 
+  /// @param words: the list of words that the user has signed
+  /// @return a Text widget that displays the most used word by the user
+  Text getMostUsedWord(List<String> words) {
+    Map<String, int> wordCount = {};
+
+    // iterate the words with a map, incrementing the count of each word
+    for (var word in words) { 
+      wordCount[word] = (wordCount[word] ?? 0) + 1;
+    }
+
+    // find the most used word
+    String mostUsedWord = "";
+    int maxCount = 0;
+    
+    // iterate every map element and find the most used word
+    wordCount.forEach((word, int count) {
+      if (count > maxCount) {
+        maxCount = count;
+        mostUsedWord = word;
+      }
+    });
+
+    // if no words are signed, display a message
+    if (mostUsedWord == ""){
+      return const Text("No words signed yet!",
+          style: TextStyle(fontSize: 20, color: Colors.white),
+          textAlign: TextAlign.center,
+        );
+    } else { 
+      // otherwise, return the most used word as a widget
+      return Text(mostUsedWord,
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+          textAlign: TextAlign.center,
+        );
+    }
+  } // end getMostUsedWord
+}
+
+// class AnalyticsScreenStateOld extends State<AnalyticsScreen> {
+//   @override
+//   void initState() {
+//     super.initState();
+//   }
+
+//   final DbService _db_service = DbService();
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Stack(
+//         children: [
+//           Positioned.fill(
+//             child: Opacity(
+//               opacity: 0.6, // set the opacity of the background image
+//               child: Image.asset('assets/images/temp-splash.jpg',
+//               fit: BoxFit.cover),
+//             ),
+//           ),
+//           Align(
+//             alignment: Alignment.center,
+//             child: Column(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 // gets the number of users from the db and displays it on screen
+//                 StreamBuilder(
+//                   stream: _db_service.getUser(auth.currentUser?.uid), // gets the user that is signed in
+//                   builder: (context, snapshot){
+//                     if (!snapshot.hasData) {
+//                       return const CircularProgressIndicator();
+//                     } else {
+//                       // display the user's name from the db
+//                       DocumentSnapshot<Object?>? doc = snapshot.data;
+
+//                       // get the list of words from the user document
+//                       List<String> words = doc?.get('words').cast<String>() ?? [];
+                      
+//                       return listOfStats(doc,words);
+//                     }
+//                   }, 
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//       // add our bottom tab bar
+//       bottomNavigationBar: const BottomTabBar(),
+//     );
+//   }
+// }
